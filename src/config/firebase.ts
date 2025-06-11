7f9011f071c619620468f5c16781ccd3964e173e
Suyash Gupta <suyashgupta2411@gmail.com>
2025-06-11 15:44:40 +0530

    Ready for deployment

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,15 +1,15 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5GPfQ-t68zN3i8MlfduXqR49I9LNwY8Y",
-  authDomain: "solmate-e4c8f.firebaseapp.com",
-  projectId: "solmate-e4c8f",
-  storageBucket: "solmate-e4c8f.firebasestorage.app",
-  messagingSenderId: "573616664483",
-  appId: "1:573616664483:web:7792d36bab872ee0aa3416",
-  measurementId: "G-R3H99X09LR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
