a519024fba1247a95dce3a90360ff7496f752714
Suyash Gupta <suyashgupta2411@gmail.com>
2025-06-17 16:24:09 +0530

    Mirating from Phantom to solflare

--- src/contexts/WalletContext.tsx
+++ src/contexts/WalletContext.tsx
@@ -5,10 +5,7 @@ import {
   useWallet,
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+import { SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 import { useEffect } from "react";
@@ -19,10 +16,7 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 const network = WalletAdapterNetwork.Devnet;
 const endpoint = clusterApiUrl(network);
 
-const wallets = [
-  new PhantomWalletAdapter(),
-  new SolflareWalletAdapter({ network }),
-];
+const wallets = [new SolflareWalletAdapter({ network })];
 
 interface WalletContextProviderProps {
   children: ReactNode;
